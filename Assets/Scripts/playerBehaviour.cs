d4d64766e2a0d1b89016ddeea7cae4fb64a08473
Rajesh Pujara <rapajara@gmail.com>
2020-09-26 01:03:53 -0400

    adding player movement and camera follow and spikes

---
+++ Assets/Scripts/playerBehaviour.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerBehaviour : MonoBehaviour
+{
+    float moveSpeed;
+    Rigidbody2D rb;
+    Vector3 facing;
+    float horizontalInput;
+    bool isJump;
+    float jumpMult;
+    float fallMultiplier;
+    float jumpWeight;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        facing = Vector3.right;
+        moveSpeed = 5.0f;
+        jumpMult = 5.0f;
+        fallMultiplier = 2.0f;
+        jumpWeight = 3.0f;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       horizontalInput = Input.GetAxisRaw("Horizontal");
+       isJump = Input.GetKeyDown(KeyCode.Space);
+       Debug.Log(isGrounded());
+       if(isJump && isGrounded()){
+            Debug.Log("jumping");
+            rb.AddForce(Vector2.up * jumpMult, ForceMode2D.Impulse);
+        }
+    }
+
+    private void FixedUpdate() {
+        //moving player left and right
+        if(horizontalInput > 0)
+            transform.Translate(Vector3.right * Time.deltaTime * moveSpeed);
+        if(horizontalInput < 0)
+            transform.Translate(Vector3.left * Time.deltaTime * moveSpeed);
+        
+        if (rb.velocity.y < jumpWeight)
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime; //makes the player fall faster after they've reached the peak of their jump, will wait until the true peak if jumpWeight is set to 0.0f
+    }
+    bool isGrounded()
+    {
+        RaycastHit2D hitGround = Physics2D.Raycast(transform.position + Vector3.down + new Vector3 (0, 0.25f, 0), Vector2.down, 0.001f); //raycasting to try to hit a platform
+
+        if ((hitGround.collider != null) && (hitGround.transform.tag == "Platform"))
+            return true;
+        else
+            return false;
+    }
+    private void OnCollisionEnter2D(Collision2D collider) {
+
+        if (collider.gameObject.tag == "Spike")
+            Destroy(gameObject); //This will straight up destroy the player, can replace this line with any code we want to execute when player touches spikes
+        
+    }
+}
---
+++ Assets/Scripts/playerFollow.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerFollow : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 cameraBox;
+    void Update()
+    {
+        //this will cause the camera to follow the player within the bounds of the "box" defined by the cameraBox variable
+        //the box is cameraBox.x*2 wide and cameraBox.y*2 tall
+        float x = transform.position.x;
+        float y = transform.position.y;
+       if(player.position.x > transform.position.x + cameraBox.x)
+            x = player.position.x - cameraBox.x;
+       else if(player.position.x < transform.position.x - cameraBox.x)
+            x = player.position.x + cameraBox.x;
+
+       if(player.position.y > transform.position.y + cameraBox.y)
+            y = player.position.y - cameraBox.y;
+       else if(player.position.y < transform.position.y - cameraBox.y)
+            y = player.position.y + cameraBox.y;
+
+        transform.position = new Vector3(x, y, -10);
+    }
+}
