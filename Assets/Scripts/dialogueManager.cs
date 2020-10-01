dd688e3d107e6727016000abb4d16536fb6754ef
Rajesh Pujara <rapajara@gmail.com>
2020-10-01 01:49:57 -0400

    adding dialogue system

---
+++ Assets/Scripts/dialogueManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class dialogueManager : MonoBehaviour
+{
+
+    public GameObject dialogueCanvas;
+    public Text nameText;
+    public Text dialogueText;
+    private Queue<string> sentences;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();    
+    }
+
+
+    public void startDialogue(Dialogue dialogue){
+
+        dialogueCanvas.SetActive(true);
+
+        nameText.text = dialogue.name;
+        sentences.Clear();
+
+        foreach(string sentence in dialogue.sentences){
+            sentences.Enqueue(sentence);
+        }
+
+        displayNextSentence();
+        
+    }
+
+    public void displayNextSentence(){
+        if(sentences.Count == 0){
+            endDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+        
+    }
+
+    void endDialogue(){
+        dialogueCanvas.SetActive(false);
+
+        Debug.Log("ending dialogue");
+
+    }
+
+}
---
+++ Assets/Scripts/dialogueTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dialogueTrigger : MonoBehaviour
+{
+
+
+    public Dialogue dialogue;
+
+    public void TriggerDialogue(){
+        FindObjectOfType<dialogueManager>().startDialogue(dialogue);
+    }
+
+    private void OnTriggerStay2D(Collider2D other) {
+        if(other.CompareTag("Player") && Input.GetKeyDown(KeyCode.Q)){
+            TriggerDialogue();
+        }
+    }
+}
